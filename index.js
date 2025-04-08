b3eb3377dfd5859e92cc5a62c89d1f74d8ccdc3b
sachin jha <sahilrodies000@gmail.com>
2025-04-08 19:20:22 +0530

    step7 -only start if database is ready

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const {userRouter}=require("./routes/user");
 const {courseRouter}=require("./routes/course");
 const {adminRouter}=require("./routes/admin");
@@ -15,10 +16,15 @@ const app=express();
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/course",courseRouter);
+async function main(){
+    await mongoose.connect("mongodb+srv://sahilrodies000:Sj%4025092002@cluster0.m8rgpgt.mongodb.net/coursera-app")
+    app.listen(3000);
+    console.log("listening on the port 3000");
+}
+main();
 
 
 
 
 
 
-app.listen(3000);
